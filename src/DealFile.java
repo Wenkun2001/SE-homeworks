943e54296fcf98fb84602fa0f25371d7c2e55e55
lwk <2470174271@qq.com>
2022-03-19 18:45:32 +0800

    命令行输入文件名，指定省份输出信息，计算总数，排序

--- src/DealFile.java
+++ src/DealFile.java
@@ -2,41 +2,118 @@
  * @ClassName DealFile
  * @Description TODO
  * @Author Wenkun Lei
- * @Date 2022/3/10 14:53
- * @Version 1.0
+ * @Date 2022/3/19 14:26
+ * @Version 3.0
  **/
 
 import java.io.*;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Locale;
+
+class CityComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+        CityData e1 = (CityData) o1;
+        CityData e2 = (CityData) o2;
+        if (e1.getPeopleNum() < e2.getPeopleNum()) {
+            return 1;
+        } else if (e1.getPeopleNum() > e2.getPeopleNum()) {
+            return -1;
+        } else {
+            return Collator.getInstance(Locale.CHINESE).compare(e1.getCityName(), e2.getCityName());
+        }
+    }
+}
+
+class ProvinceComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+        ProvinceData e1 = (ProvinceData) o1;
+        ProvinceData e2 = (ProvinceData) o2;
+        if (e1.getPeopleNum() < e2.getPeopleNum())
+            return 1;
+        else if (e1.getPeopleNum() > e2.getPeopleNum()) {
+            return -1;
+        } else {
+            return Collator.getInstance(Locale.CHINESE).compare(e1.getProvinceName(), e2.getProvinceName());
+        }
+    }
+}
 
 public class DealFile {
+    public String inputFile;
+    public String outputFile;
+    public String outputFile2;
+
+    public DealFile() {
+
+    }
+
+    public DealFile(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+
+    public DealFile(String inputFile, String outputFile, String outputFile2) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+        this.outputFile2 = outputFile2;
+    }
+
     //  处理文件格式，输入参数依次为文件的编码格式、待处理的输入文件的路径、处理完成的输出文件的路径
-    public void dealTxtFile(String encoding, String inputFile, String outputFile) {
+    public void dealTxtFile(String designatedProvince) throws IOException {
+        ArrayList provinceDataList = null;
+        ArrayList singleDataList = null;
         try {
-            File file = new File(inputFile);
+            File file = new File(this.inputFile);
             //  判断文件是否存在
             if (file.isFile() && file.exists()) {
+                String encoding = getFileFormat(this.inputFile);
                 //  考虑到编码格式，使用对应编码格式将文件输入流加载为输入读取流
                 InputStreamReader read = new InputStreamReader(new FileInputStream(file), encoding);
                 //  创建文件读取缓冲区
                 BufferedReader br = new BufferedReader(read);
                 //  创建文件输出流
-                FileWriter fw = new FileWriter(outputFile);
+                FileWriter fw = new FileWriter(this.outputFile);
                 //  创建文件写入缓冲区
                 BufferedWriter bw = new BufferedWriter(fw);
                 //  创建读取缓冲区的每行的字符串变量
                 String lineTxt = null;
                 //  创建每行数据的省份字符串变量
                 String currentProvince = "0";
+
+                int cityPeopleNum;
+                int totalPeopleNum = 0;
+
+                int countFlag = 0;
+
+                provinceDataList = new ArrayList<>();
                 //  缓冲区有数据就一直一行一行地读取
                 while ((lineTxt = br.readLine()) != null) {
                     //  使用空格作为分割符，将每行的字符串分割为字符串数组
                     String[] lineArray = lineTxt.split("\\s+");
+                    if (!lineArray[0].equals(designatedProvince) && !designatedProvince.equals("")) {
+                        continue;
+                    }
                     //  略去“待明确地区”
-                    if(lineArray[1].equals("待明确地区")) {
+                    if (lineArray[1].equals("待明确地区")) {
                         continue;
                     }
                     //  将当前城市的省份进行比较，如果省份不一样了，那么将对当前省份进行替换
                     if (!currentProvince.equals(lineArray[0])) {
+                        if (countFlag == 0) {
+                            singleDataList = new ArrayList<>();
+                            countFlag = 1;
+                        } else {
+                            bw.write("总计: " + totalPeopleNum);
+//                            System.out.println(currentProvince);
+                            ProvinceData provinceData = new ProvinceData(currentProvince, totalPeopleNum, singleDataList);
+                            provinceDataList.add(provinceData);
+                            singleDataList = new ArrayList<>();
+                            totalPeopleNum = 0;
+                            bw.newLine();
+                        }
                         if (!currentProvince.equals("0")) {
                             //  写入一个换行符
                             bw.newLine();
@@ -48,10 +125,20 @@ public class DealFile {
                     }
                     //  向缓冲区写入除了省份的其余数据
                     bw.write(lineArray[1] + "\t" + lineArray[2]);
+                    cityPeopleNum = Integer.parseInt(lineArray[2]);
+                    totalPeopleNum += cityPeopleNum;
+                    CityData cityData = new CityData(lineArray[1], cityPeopleNum);
+                    singleDataList.add(cityData);
                     //  写入一个换行符
                     bw.newLine();
                     bw.flush();
                 }
+                bw.write("总计: " + totalPeopleNum);
+//                System.out.println(currentProvince);
+                ProvinceData provinceData = new ProvinceData(currentProvince, totalPeopleNum, singleDataList);
+                provinceDataList.add(provinceData);
+
+                bw.flush();
                 // 关闭io流
                 bw.close();
                 read.close();
@@ -64,6 +151,45 @@ public class DealFile {
             System.out.println("读取文件内容出错");
             e.printStackTrace();
         }
+        sortCity(provinceDataList);
+    }
+
+
+    public void sortCity(ArrayList provinceDataList) throws IOException {
+        //  创建文件输出流
+        FileWriter fw = new FileWriter(this.outputFile2);
+        //  创建文件写入缓冲区
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        //必须是Comparator中的compare方法和Collections.sort方法配合使用才管用
+        ProvinceComparator pct = new ProvinceComparator();
+        CityComparator cct = new CityComparator();
+        Comparator cmp = Collator.getInstance(java.util.Locale.CHINA);
+        Collections.sort(provinceDataList, pct);
+
+        ArrayList cityList = null;
+
+        for (Object o : provinceDataList) {
+            ProvinceData provinceData = (ProvinceData) o;
+            cityList = provinceData.getCityDataList();
+
+            cityList.sort(cct);
+            bw.write(provinceData.getProvinceName() + "\t" + provinceData.getPeopleNum());
+            //  写入一个换行符
+            bw.newLine();
+
+            for (Object value : cityList) {
+                CityData cityData = (CityData) value;
+                bw.write(cityData.getCityName() + "\t" + cityData.getPeopleNum());
+                //  写入一个换行符
+                bw.newLine();
+            }
+            //  写入一个换行符
+            bw.newLine();
+            bw.flush();
+        }
+        // 关闭io流
+        bw.close();
     }
 
 
@@ -72,7 +198,6 @@ public class DealFile {
         File file = new File(filePath);
         InputStream inputStream = new FileInputStream(file);
         byte[] head = new byte[3];
-        inputStream.read(head);
         String code = "GBK";
         if (head[0] == -1 && head[1] == -2) {
             code = "UTF-16";
@@ -89,11 +214,15 @@ public class DealFile {
     }
 
 
-    public static void main(String[] args) throws Exception {
-        String inputFile = "src/yq_in.txt";
-        String outputFile = "src/my_yq_out.txt";
-        DealFile rf = new DealFile();
-        String encoding = rf.getFileFormat(inputFile);
-        rf.dealTxtFile(encoding, inputFile, outputFile);
+    public static void main(String[] args) throws IOException {
+//        String inputFile = args[0];
+//        String outputFile = args[1];
+//        String designatedProvince = args[2];
+        String inputFile = "src/yq_in_03.txt";
+        String outputFile = "src/my_yq_out_03.txt";
+        String outputFile2 = "src/my_yq_out_04.txt";
+        String designatedProvince = "";
+        DealFile rf = new DealFile(inputFile, outputFile, outputFile2);
+        rf.dealTxtFile(designatedProvince);
     }
 }
